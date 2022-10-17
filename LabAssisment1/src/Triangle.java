8455481573082895930063ef065cb164667488a4
lenovo <lenovo@DESKTOP-FBE5NAR>
2022-10-17 14:00:50 +0530

    My First Commit

---
+++ LabAssisment1/src/Triangle.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Triangle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Enter First Angle");
+		Scanner obj=new Scanner(System.in);
+		int x=obj.nextInt();
+		System.out.println("Enter second Angle");
+		int y=obj.nextInt();
+		System.out.println("Enter Third Angle");
+		int z=obj.nextInt();
+		int a=x+y+z;
+		if(a==180)
+		{
+			System.out.println("1st Price");
+		}
+
+		else 
+		{
+			System.out.println("No prize");
+		}     
+	}
+
+	}
+
